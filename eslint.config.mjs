f48de4c48ab83878ece7bf7c034e3380cabb60a2
SaltySalt77 <hyna0707@gmail.com>
2025-07-18 00:28:27 +0900

    Chore: 프로젝트 기본 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,4 @@
+// .eslintrc.mjs
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -5,12 +6,43 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+const compat = new FlatCompat({ baseDirectory: __dirname });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
+  ...compat.extends(
+    "next/core-web-vitals",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier"
+  ),
+  {
+    rules: {
+      // Prettier 관련
+      "prettier/prettier": "error",
+      // unused-vars
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": ["error"],
+      // explicit returns
+      "@typescript-eslint/explicit-function-return-type": ["warn"],
+      // no any
+      "@typescript-eslint/no-explicit-any": ["error"],
+      // React-specific
+      "react/no-unescaped-entities": "off",
+      "react/display-name": "off",
+      "react/prop-types": "off",
+      // 모든 JSX prop 값을 중괄호로 감싸도록 강제
+      "react/jsx-curly-brace-presence": [
+        "error",
+        { props: "always", children: "never" },
+      ],
+    },
+    env: {
+      browser: true,
+      node: true,
+      es6: true,
+    },
+    settings: {
+      react: { version: "detect" },
+    },
+  },
 ];
-
-export default eslintConfig;
