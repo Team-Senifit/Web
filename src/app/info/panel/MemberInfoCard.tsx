10ced6b1b97e6a02ac68e58e534ef8c26755224a
SaltySalt77 <hyna0707@gmail.com>
2025-07-27 19:29:20 +0900

    Feat: 센터 정보 카드들 생성

---
+++ src/app/info/panel/MemberInfoCard.tsx
@@ -0,0 +1,61 @@
+import { Button, Divider, Stack, Typography } from "@mui/material";
+import React from "react";
+
+const Member = ({ name, age, grade, gender }: IMember) => {
+  return (
+    <Stack direction={"row"} spacing={3}>
+      <Typography>{name}</Typography>
+      <Stack direction={"row"} spacing={1}>
+        <Typography>{age}세</Typography>
+        <Typography>{grade}</Typography>
+        <Typography>{gender} </Typography>
+      </Stack>
+    </Stack>
+  );
+};
+
+const MemberInfoCard = ({
+  count,
+  members,
+}: {
+  count: number;
+  members: Array<IMember>;
+}) => {
+  return (
+    <Stack
+      sx={{
+        bgcolor: "lightgray",
+        borderRadius: 1,
+        padding: 2,
+        width: 1,
+      }}
+    >
+      <Stack
+        direction="row"
+        justifyContent="space-between"
+        alignItems="start"
+        spacing={1}
+      >
+        <Stack direction={"column"} spacing={1}>
+          <Typography>{"센터 인원"}</Typography>
+          <Typography>{"현재 등록된 어르신은"}</Typography>
+          <Typography>
+            <Typography component="span">{`${count}명`}</Typography>
+            {"입니다."}
+          </Typography>
+        </Stack>
+        <Button variant="contained">
+          <Typography>{"관리하기"}</Typography>
+        </Button>
+      </Stack>
+      <Divider sx={{ width: "50%" }} />
+      <Stack spacing={2}>
+        {members.map((member) => (
+          <Member key={member.id} {...member} />
+        ))}
+      </Stack>
+    </Stack>
+  );
+};
+
+export default MemberInfoCard;
---
+++ src/types/IMember.ts
@@ -0,0 +1,7 @@
+interface IMember {
+  id: number;
+  name: string;
+  age: number;
+  grade: "1등급" | "2등급" | "3등급" | "4등급" | "5등급" | "인지지원등급";
+  gender: "여성" | "남성";
+}
