c0400253adad2c2b6a917fd0c00184b2b41383ab
SaltySalt77 <hyna0707@gmail.com>
2025-07-27 16:48:00 +0900

    Chore: Storybook 생성

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 // .eslintrc.mjs
 import { dirname } from "path";
 import { fileURLToPath } from "url";
@@ -8,41 +11,38 @@ const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({ baseDirectory: __dirname });
 
-export default [
-  ...compat.extends(
-    "next/core-web-vitals",
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier"
-  ),
-  {
-    rules: {
-      // Prettier 관련
-      "prettier/prettier": "error",
-      // unused-vars
-      "no-unused-vars": "off",
-      "@typescript-eslint/no-unused-vars": ["error"],
-      // explicit returns
-      "@typescript-eslint/explicit-function-return-type": ["warn"],
-      // no any
-      "@typescript-eslint/no-explicit-any": ["error"],
-      // React-specific
-      "react/no-unescaped-entities": "off",
-      "react/display-name": "off",
-      "react/prop-types": "off",
-      // 모든 JSX prop 값을 중괄호로 감싸도록 강제
-      "react/jsx-curly-brace-presence": [
-        "error",
-        { props: "always", children: "never" },
-      ],
-    },
-    env: {
-      browser: true,
-      node: true,
-      es6: true,
-    },
-    settings: {
-      react: { version: "detect" },
-    },
+export default [...compat.extends(
+  "next/core-web-vitals",
+  "eslint:recommended",
+  "plugin:@typescript-eslint/recommended",
+  "prettier"
+), {
+  rules: {
+    // Prettier 관련
+    "prettier/prettier": "error",
+    // unused-vars
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": ["error"],
+    // explicit returns
+    "@typescript-eslint/explicit-function-return-type": ["warn"],
+    // no any
+    "@typescript-eslint/no-explicit-any": ["error"],
+    // React-specific
+    "react/no-unescaped-entities": "off",
+    "react/display-name": "off",
+    "react/prop-types": "off",
+    // 모든 JSX prop 값을 중괄호로 감싸도록 강제
+    "react/jsx-curly-brace-presence": [
+      "error",
+      { props: "always", children: "never" },
+    ],
   },
-];
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  settings: {
+    react: { version: "detect" },
+  },
+}, ...storybook.configs["flat/recommended"]];
