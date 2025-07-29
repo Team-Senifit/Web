4519efe6004775aaac24f93f3da7ab1e9687672d
SaltySalt77 <hyna0707@gmail.com>
2025-07-29 21:30:13 +0900

    Docs: SenifitTextField 위치 변경

--- src/stories/components/SenifitTextField.stories.tsx
+++ src/stories/components/SenifitTextField.stories.tsx
@@ -3,7 +3,7 @@ import SenifitTextField from "@/components/SenifitTextField";
 import WithRHF from "../utils/WithRHF"; // 경로는 상황에 따라 조정
 
 const meta: Meta<typeof SenifitTextField> = {
-  title: "Forms/SenifitTextField",
+  title: "Components/SenifitTextField",
   component: SenifitTextField,
   decorators: [WithRHF(false)], // submit 버튼 숨김 여부 설정
   tags: ["autodocs"],
