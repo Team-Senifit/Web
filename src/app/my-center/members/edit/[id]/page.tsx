660ee8153f44441fd49aa3c0f2fe7f7685447a80
SaltySalt77 <hyna0707@gmail.com>
2025-08-03 23:19:19 +0900

    Rename: 탭 명에 따라 routing 변경

---
+++ src/app/my-center/members/edit/[id]/page.tsx
@@ -0,0 +1,19 @@
+import EditForm from "@/app/my-center/panel/EditForm";
+import React from "react";
+
+const Page = ({ params: { id } }: { params: { id: string } }) => {
+  const data: IMember = {
+    id: parseInt(id),
+    name: "홍길동",
+    age: 70,
+    grade: "1등급",
+    gender: "남성",
+  };
+  return (
+    <div>
+      <EditForm isEdit defaultValues={data} />
+    </div>
+  );
+};
+
+export default Page;
---
+++ src/app/my-center/panel/InfoCard.tsx
@@ -0,0 +1,23 @@
+import { Box, Stack, Typography } from "@mui/material";
+import React from "react";
+
+const InfoCard = ({ title, content }: { title: string; content: string }) => {
+  return (
+    <Stack
+      component={"section"}
+      direction="column"
+      spacing={1}
+      sx={{
+        bgcolor: "lightgray",
+        borderRadius: 1, // mui 기준 8px
+        padding: 2,
+        width: 1, // width, height에서는 1이 100%를 의미
+      }}
+    >
+      <Typography>{title}</Typography>
+      <Typography>{content}</Typography>
+    </Stack>
+  );
+};
+
+export default InfoCard;
