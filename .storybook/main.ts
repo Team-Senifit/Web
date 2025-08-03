c1f77a080d80e651e696eb0728076352617701f5
SaltySalt77 <hyna0707@gmail.com>
2025-08-04 01:31:15 +0900

    Docs: 아이콘 스토리북에 추가

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+import TsconfigPathsPlugin from "tsconfig-paths-webpack-plugin";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -11,5 +12,16 @@ const config: StorybookConfig = {
     options: {},
   },
   staticDirs: ["../public"],
+  webpackFinal: async (config) => {
+    if (config.resolve) {
+      config.resolve.plugins = [
+        ...(config.resolve.plugins || []),
+        new TsconfigPathsPlugin({
+          configFile: "./tsconfig.json",
+        }),
+      ];
+    }
+    return config;
+  },
 };
 export default config;
