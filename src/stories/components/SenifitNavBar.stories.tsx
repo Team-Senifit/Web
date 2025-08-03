83385feef29f31536aa6ad1236180419cdd76eb8
SaltySalt77 <hyna0707@gmail.com>
2025-08-04 00:30:42 +0900

    Feat: 데스크탑에서 안보이게 처리

--- src/stories/components/SenifitNavBar.stories.tsx
+++ src/stories/components/SenifitNavBar.stories.tsx
@@ -4,14 +4,6 @@ import SenifitNavBar from "../../components/SenifitNavBar";
 const meta: Meta = {
   title: "components/SenifitNavBar",
   component: SenifitNavBar,
-  argTypes: {
-    isStorybook: {
-      control: "boolean",
-      description:
-        "스토리북에서 사용 여부.<br />docs에서 잘 보려면 true로 설정",
-      defaultValue: false,
-    },
-  },
 };
 
 export default meta;
@@ -19,7 +11,5 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
-  args: {
-    isStorybook: false,
-  },
+  args: {},
 };
