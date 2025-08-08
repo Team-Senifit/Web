70a4da505719c2a5ebc490b3ae6a29ba218eeac1
SaltySalt77 <hyna0707@gmail.com>
2025-08-08 16:56:05 +0900

    Design: 아이콘 strokeWidth 변경

--- src/components/icons/HumanIcon.tsx
+++ src/components/icons/HumanIcon.tsx
@@ -1,8 +1,9 @@
 "use client";
+import * as React from "react";
+import { createSvgIcon } from "@mui/material/utils";
+import type { SvgIconProps } from "@mui/material/SvgIcon";
 
-import { createSvgIcon } from "@mui/material";
-
-const HumanIcon = createSvgIcon(
+const HumanIconBase = createSvgIcon(
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="23"
@@ -13,19 +14,28 @@ const HumanIcon = createSvgIcon(
     <path
       d="M20.4452 6.6001C21.4704 6.6001 21.7733 7.83784 20.8399 8.21379L14.3603 10.8241L17.0843 19.8322C17.3727 20.7863 15.8952 21.3285 15.3378 20.4733L11.5003 14.5836L7.66275 20.4733C7.10539 21.3287 5.62774 20.7864 5.91622 19.8322L8.64029 10.8241L2.15969 8.21379C1.22691 7.83766 1.52948 6.60024 2.55443 6.6001H20.4452Z"
       stroke="currentColor"
-      strokeWidth="1.5"
       strokeLinecap="round"
       strokeLinejoin="round"
     />
-    <circle
-      cx="11.5"
-      cy="3.35725"
-      r="2.25715"
-      stroke="currentColor"
-      strokeWidth="1.5"
-    />
+    <circle cx="11.5" cy="3.35725" r="2.25715" stroke="currentColor" />
   </svg>,
-  "HumanIcon"
+  "HumanIconBase"
 );
+
+export type HumanIconProps = SvgIconProps & {
+  strokeWidth?: number | string;
+};
+
+const HumanIcon = React.forwardRef<SVGSVGElement, HumanIconProps>(
+  ({ strokeWidth = 1.5, sx, ...rest }, ref) => (
+    <HumanIconBase
+      ref={ref}
+      sx={{ ...sx, strokeWidth, "& *": { vectorEffect: "non-scaling-stroke" } }}
+      {...rest}
+    />
+  )
+);
+
+HumanIcon.displayName = "HumanIcon";
 
 export default HumanIcon;
