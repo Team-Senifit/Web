91d785f71312ec853c6a43da7a9bf134ac19e216
SaltySalt77 <hyna0707@gmail.com>
2025-08-08 18:24:32 +0900

    Refactor: MemberCard 모바일 기준으로 변경

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -2,3 +2,4 @@ export { default as HumanIcon } from "./HumanIcon";
 export { default as ClipboardIcon } from "./ClipboardIcon";
 export { default as HouseIcon } from "./HouseIcon";
 export { default as MapPinHouseIcon } from "./MapPinHouseIcon";
+export { default as SquareUserRoundIcon } from "./SquareUserRoundIcon";
