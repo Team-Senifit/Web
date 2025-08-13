ff19bde4145ed0b75ad9e3a8022c4aa876fd9c10
SaltySalt77 <hyna0707@gmail.com>
2025-08-13 15:53:58 +0900

    Refactor: 자주 쓰는 컴포넌트 정리

--- src/app/my-center/members/page.tsx
+++ src/app/my-center/members/page.tsx
@@ -38,7 +38,7 @@ const Page = () => {
   const { isDesktop } = useMedia();
   return (
     <Stack spacing={[2, 3]} sx={{ width: "100%", height: "100%" }}>
-      <ReturnButton />
+      <ReturnButton href="/my-center" />
       <Stack
         direction={"column"}
         justifyContent="start"
