e259c2d7bf8219ec2d3ed213b208345d5f93fd37
SaltySalt77 <hyna0707@gmail.com>
2025-08-19 19:55:34 +0900

    Feat: 어르신 등급 토글 버튼 추가

--- src/app/my-center/members/panel/birth-date-picker/LunarSolarToggle.tsx
+++ src/app/my-center/members/panel/birth-date-picker/LunarSolarToggle.tsx
@@ -43,6 +43,7 @@ const LunarSolarToggle = ({
       }}
     >
       <SenifitToggleButtonGroup<CalendarType>
+        fullWidth
         value={calendarType}
         onChange={setCalendarType}
         exclusive
