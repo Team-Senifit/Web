9fd2612c8d179ec12b29948af95042e9f9dd1126
SaltySalt77 <hyna0707@gmail.com>
2025-09-01 02:40:18 +0900

    Feat: api 연동 및 선택 데이터 저장

--- src/states/useProgramStore.ts
+++ src/states/useProgramStore.ts
@@ -3,18 +3,22 @@ import { IRoutineDetail } from "@/types/IRoutineDetail";
 import { create } from "zustand";
 
 interface IProgramStore {
+  id: number | null;
   selectedProgram: IRoutineDetail | null;
   selectedMembers: IMember[] | null;
   type: "customized" | "popular" | "thematic" | null;
+  setId: (id: number | null) => void;
   setSelectedProgram: (program: IRoutineDetail | null) => void;
   setSelectedMembers: (members: IMember[] | null) => void;
   setType: (type: "customized" | "popular" | "thematic" | null) => void;
 }
 
 const useProgramStore = create<IProgramStore>((set) => ({
+  id: null,
   selectedProgram: null,
   selectedMembers: null,
   type: null,
+  setId: (id) => set({ id }),
   setSelectedProgram: (program) => set({ selectedProgram: program }),
   setSelectedMembers: (members) => set({ selectedMembers: members }),
   setType: (type) => set({ type }),
