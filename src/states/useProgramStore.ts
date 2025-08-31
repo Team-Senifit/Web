7f10b61b5389ebb6159cdc77142e82f31947c7ce
SaltySalt77 <hyna0707@gmail.com>
2025-09-01 02:01:24 +0900

    Feat: 전역상태에 타입 추가

--- src/states/useProgramStore.ts
+++ src/states/useProgramStore.ts
@@ -5,15 +5,19 @@ import { create } from "zustand";
 interface IProgramStore {
   selectedProgram: IRoutineDetail | null;
   selectedMembers: IMember[] | null;
+  type: "customized" | "popular" | "thematic" | null;
   setSelectedProgram: (program: IRoutineDetail | null) => void;
   setSelectedMembers: (members: IMember[] | null) => void;
+  setType: (type: "customized" | "popular" | "thematic" | null) => void;
 }
 
 const useProgramStore = create<IProgramStore>((set) => ({
   selectedProgram: null,
   selectedMembers: null,
+  type: null,
   setSelectedProgram: (program) => set({ selectedProgram: program }),
   setSelectedMembers: (members) => set({ selectedMembers: members }),
+  setType: (type) => set({ type }),
 }));
 
 export default useProgramStore;
