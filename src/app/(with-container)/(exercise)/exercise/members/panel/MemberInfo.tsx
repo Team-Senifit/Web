9213da2cfc60a633238b1604bab9a767c27e31ad
SaltySalt77 <hyna0707@gmail.com>
2025-09-01 04:33:49 +0900

    Design: Divider에 색상 제대로 안들어간 현상

--- src/app/(with-container)/(exercise)/exercise/members/panel/MemberInfo.tsx
+++ src/app/(with-container)/(exercise)/exercise/members/panel/MemberInfo.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import useMedia from "@/hooks/useMedia";
 import { genderLabel, gradeLabel, IMember } from "@/types/IMember";
 import { calculateAge } from "@/utils/calculateAge";
 import { Stack, Typography } from "@mui/material";
@@ -5,15 +8,17 @@ import dayjs from "dayjs";
 import React from "react";
 
 const MemberInfo = ({ name, birthDate, gender, memberRank }: IMember) => {
+  const { isDesktop } = useMedia();
   return (
     <Stack
       direction={["column", "row"]}
       justifyContent={["flex-start", "space-between"]}
       columnGap={1}
       width={"100%"}
+      maxWidth={"33.25rem"}
     >
       <Typography
-        variant={"Headline1"}
+        variant={isDesktop ? "Title2" : "Headline1"}
         sx={{
           color: "label.normal",
         }}
@@ -21,14 +26,20 @@ const MemberInfo = ({ name, birthDate, gender, memberRank }: IMember) => {
         {name}
       </Typography>
       <Stack direction={"row"} spacing={2}>
-        <Typography variant={"Headline1"} sx={{ color: "label.neutral" }}>
+        <Typography
+          variant={isDesktop ? "Heading1" : "Headline1"}
+          sx={{ color: "label.neutral" }}
+        >
           {`${calculateAge(dayjs(birthDate))}세`}
         </Typography>
-        <Typography variant={"Headline1"} sx={{ color: "label.neutral" }}>
+        <Typography
+          variant={isDesktop ? "Heading1" : "Headline1"}
+          sx={{ color: "label.neutral" }}
+        >
           {genderLabel[gender]}
         </Typography>
         <Typography
-          variant={"Headline1"}
+          variant={isDesktop ? "Heading1" : "Headline1"}
           sx={{ color: "label.neutral", width: "6rem" }}
         >
           {gradeLabel[memberRank]}
