805662ab2578e6d6902bd2f7f26eeee95fe4c12f
SaltySalt77 <hyna0707@gmail.com>
2025-09-01 16:20:54 +0900

    Feat: 상단 타이머 구현

---
+++ src/app/(plain)/exercise/start/panel/Header.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import { Button, Stack, Typography } from "@mui/material";
+import React, { useState } from "react";
+import Timer from "./Timer";
+import useProgramStore from "@/states/useProgramStore";
+import { useRouter } from "next/navigation";
+import useMedia from "@/hooks/useMedia";
+
+const Header = () => {
+  const { isPhone, isTablet } = useMedia();
+  const router = useRouter();
+
+  const { selectedProgram } = useProgramStore();
+
+  const [seconds, setSeconds] = useState<number>(0);
+
+  if (!selectedProgram) {
+    router.push("/");
+    return null;
+  }
+  return (
+    <Stack
+      direction={"row"}
+      sx={{
+        py: 3,
+        px: 4.5,
+        bgcolor: "background.paper",
+      }}
+      justifyContent={"space-between"}
+    >
+      <Stack
+        direction={"row"}
+        alignItems={"center"}
+        spacing={1}
+        sx={{
+          borderLeft: "4px solid",
+          borderColor: "primary.main",
+          pl: 1,
+          py: 1,
+        }}
+      >
+        <Typography
+          variant={isPhone ? "Headline1" : "Heading1"}
+          sx={{
+            color: "labelVariants.normal",
+          }}
+        >
+          {"수업 시간"}
+        </Typography>
+        <Timer
+          duration={selectedProgram?.duration}
+          seconds={seconds}
+          setSeconds={setSeconds}
+          variant={isPhone ? "Headline1" : isTablet ? "Title2" : "Title1"}
+        />
+      </Stack>
+      {isPhone ? (
+        <Button
+          sx={{
+            px: 2,
+            py: 1,
+            bgcolor: "fillVariants.negative",
+            color: "statusVariants.negative",
+          }}
+        >
+          <Typography variant={"Headline1"}>{"종료"}</Typography>
+        </Button>
+      ) : (
+        <Button
+          sx={{
+            px: 8,
+            py: 2,
+            bgcolor: "fillVariants.negative",
+            color: "statusVariants.negative",
+            borderRadius: "0.75rem",
+          }}
+        >
+          <Typography variant={"Heading1"}>{"수업 중단"}</Typography>
+        </Button>
+      )}
+    </Stack>
+  );
+};
+
+export default Header;
