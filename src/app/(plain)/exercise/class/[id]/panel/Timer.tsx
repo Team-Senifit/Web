9d158b0e8147e69109015207cf070802eb48ddd9
SaltySalt77 <hyna0707@gmail.com>
2025-09-05 19:26:07 +0900

    Refactor: 타이머 리팩토링

--- src/app/(plain)/exercise/class/[id]/panel/Timer.tsx
+++ src/app/(plain)/exercise/class/[id]/panel/Timer.tsx
@@ -1,31 +1,16 @@
+import { formatTime } from "@/stories/utils/formatTime";
 import { Typography } from "@mui/material";
-import React, { useEffect } from "react";
+import React from "react";
 
 const Timer = ({
   duration,
   seconds,
-  setSeconds,
   variant,
 }: {
   duration: number;
   seconds: number;
-  setSeconds: React.Dispatch<React.SetStateAction<number>>;
   variant: "Headline1" | "Heading1" | "Title1" | "Title2";
 }) => {
-  useEffect(() => {
-    let interval: NodeJS.Timeout | null = null;
-    interval = setInterval(() => {
-      setSeconds((seconds) => seconds + 1);
-    }, 1000);
-    return () => clearInterval(interval as NodeJS.Timeout);
-  }, []);
-
-  const formatTime = (totalSeconds: number) => {
-    const minutes = Math.floor(totalSeconds / 60);
-    const secs = totalSeconds % 60;
-    return `${minutes.toString().padStart(2, "0")}:${secs.toString().padStart(2, "0")}`;
-  };
-
   return (
     <Typography
       variant={variant}
