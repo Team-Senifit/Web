9db83e9e00fc999d6ac571c8b88daf38f8f7e223
SaltySalt77 <hyna0707@gmail.com>
2025-09-11 18:28:19 +0900

    Fix: 생년월일 수정/등록 캘린더 ui가 안뜨는 현상

--- src/app/panel/SenifitThemeProvider.tsx
+++ src/app/panel/SenifitThemeProvider.tsx
@@ -284,12 +284,6 @@ const theme = createTheme({
       defaultProps: {
         disableElevation: true,
       },
-      styleOverrides: {
-        root: {
-          borderRadius: "0.75rem",
-          py: 2,
-        },
-      },
     },
   },
 });
@@ -316,21 +310,6 @@ if (theme.components) {
       },
     },
   };
-
-  if (theme.components.MuiButton) {
-    theme.components.MuiButton.styleOverrides = {
-      root: {
-        borderRadius: "0.75rem",
-        height: "3.75rem",
-      },
-      contained: {
-        boxShadow: "none",
-      },
-      text: {
-        backgroundColor: "fillVariant.colored",
-      },
-    };
-  }
 }
 
 theme.typography.Display1 = {
