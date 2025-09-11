059a8cbb309bc0eff9b228d0ca7b7917052eafe9
HYUN-SIUU <hyunsoo6542@gmail.com>
2025-09-12 01:58:01 +0900

    fix: typecheck 오류 고쳤습니다.

--- src/app/(with-container)/record/detail/[id]/page.tsx
+++ src/app/(with-container)/record/detail/[id]/page.tsx
@@ -14,6 +14,7 @@ export default async function RecordDetailPage({
 }) {
   const { id } = await params;
   const currentRecord = await getRecordServer(parseInt(id));
+  //
 
   const participantCount = currentRecord?.participantCount ?? 0;
 
