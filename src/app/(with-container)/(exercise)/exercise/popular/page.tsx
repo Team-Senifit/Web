dcaafb25e4d626caed4a3dfe5952229235d31ae5
SaltySalt77 <hyna0707@gmail.com>
2025-09-11 22:31:48 +0900

    Design: 운동탭 ReturnButton과의 갭 조정

--- src/app/(with-container)/(exercise)/exercise/popular/page.tsx
+++ src/app/(with-container)/(exercise)/exercise/popular/page.tsx
@@ -35,41 +35,43 @@ const Page = () => {
   return (
     <Stack direction={"column"} spacing={[2, 3]}>
       <ReturnButton href={"/"} />
-      <GradationPageInfoCard
-        title={"인기 운동 프로그램"}
-        description={
-          "시니핏에서 인기있는 운동 프로그램을 한 눈에!\n자세히 보기로 어떤 운동들이 있는지 확인해 보세요."
-        }
-      />
-      <Stack
-        direction={"column"}
-        spacing={[3, 4]}
-        sx={{
-          p: [3, 6],
-          bgcolor: "background.paper",
-          borderRadius: [0, "0.75rem"],
-          boxShadow: ["none", "0 0 8px 0 rgba(12, 13, 13, 0.05)"],
-          width: "100%",
-        }}
-      >
-        {popularRoutine.map((routine) => (
-          <VideoInfo
-            key={routine.id}
-            {...routine}
-            onButtonClick={() => {
-              setType("popular");
-              setSelectedRoutineRecord({
-                ...selectedRoutineRecord,
-                programId: routine.id,
-                routineKind: "workout_programs_selections_byPopular",
-                cognitiveKind: "workout_notSelected",
-                targetKind: "workout_notSelected",
-                singingKind: "workout_notSelected",
-              });
-              router.push("/exercise/members");
-            }}
-          />
-        ))}
+      <Stack direction={"column"} spacing={3}>
+        <GradationPageInfoCard
+          title={"인기 운동 프로그램"}
+          description={
+            "시니핏에서 인기있는 운동 프로그램을 한 눈에!\n자세히 보기로 어떤 운동들이 있는지 확인해 보세요."
+          }
+        />
+        <Stack
+          direction={"column"}
+          spacing={[3, 4]}
+          sx={{
+            p: [3, 6],
+            bgcolor: "background.paper",
+            borderRadius: [0, "0.75rem"],
+            boxShadow: ["none", "0 0 8px 0 rgba(12, 13, 13, 0.05)"],
+            width: "100%",
+          }}
+        >
+          {popularRoutine.map((routine) => (
+            <VideoInfo
+              key={routine.id}
+              {...routine}
+              onButtonClick={() => {
+                setType("popular");
+                setSelectedRoutineRecord({
+                  ...selectedRoutineRecord,
+                  programId: routine.id,
+                  routineKind: "workout_programs_selections_byPopular",
+                  cognitiveKind: "workout_notSelected",
+                  targetKind: "workout_notSelected",
+                  singingKind: "workout_notSelected",
+                });
+                router.push("/exercise/members");
+              }}
+            />
+          ))}
+        </Stack>
       </Stack>
     </Stack>
   );
--- src/app/(with-container)/(exercise)/exercise/thematic/[type]/page.tsx
+++ src/app/(with-container)/(exercise)/exercise/thematic/[type]/page.tsx
@@ -41,37 +41,39 @@ const Page = () => {
   return (
     <Stack direction={"column"} spacing={[2, 3]}>
       <ReturnButton href={"/exercise/thematic"} />
-      <GradationPageInfoCard
-        title={`주제별 운동 프로그램 - ${thematicWorkoutCodesLabel[params.type as WorkoutKind]}`}
-        description={
-          "하고 싶은 주제를 선택하여\n운동 프로그램을 진행할 수 있어요"
-        }
-      />
-      <Stack
-        direction={"column"}
-        spacing={[3, 4]}
-        sx={{
-          p: [3, 6],
-          bgcolor: "background.paper",
-          boxShadow: ["none", "0 0 8px 0 rgba(12, 13, 13, 0.05)"],
-          borderRadius: [0, "0.75rem"],
-          width: "100%",
-        }}
-      >
-        {routines.map((routine) => (
-          <VideoInfo
-            key={routine.id}
-            {...routine}
-            onButtonClick={() => {
-              setType(["thematic", params.type]);
-              setSelectedRoutineRecord({
-                ...selectedRoutineRecord,
-                programId: routine.id,
-              });
-              router.push("/exercise/members");
-            }}
-          />
-        ))}
+      <Stack direction={"column"} spacing={3}>
+        <GradationPageInfoCard
+          title={`주제별 운동 프로그램 - ${thematicWorkoutCodesLabel[params.type as WorkoutKind]}`}
+          description={
+            "하고 싶은 주제를 선택하여\n운동 프로그램을 진행할 수 있어요"
+          }
+        />
+        <Stack
+          direction={"column"}
+          spacing={[3, 4]}
+          sx={{
+            p: [3, 6],
+            bgcolor: "background.paper",
+            boxShadow: ["none", "0 0 8px 0 rgba(12, 13, 13, 0.05)"],
+            borderRadius: [0, "0.75rem"],
+            width: "100%",
+          }}
+        >
+          {routines.map((routine) => (
+            <VideoInfo
+              key={routine.id}
+              {...routine}
+              onButtonClick={() => {
+                setType(["thematic", params.type]);
+                setSelectedRoutineRecord({
+                  ...selectedRoutineRecord,
+                  programId: routine.id,
+                });
+                router.push("/exercise/members");
+              }}
+            />
+          ))}
+        </Stack>
       </Stack>
     </Stack>
   );
