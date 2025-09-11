d164c391e085fb7fe2d15a09c4fa8aa1dc3f8b0b
SaltySalt77 <hyna0707@gmail.com>
2025-09-11 22:46:43 +0900

    Design: 푸터 위치 조정

--- src/app/(with-footer)/layout.tsx
+++ src/app/(with-footer)/layout.tsx
@@ -2,6 +2,7 @@ import SenifitHeader from "@/components/SenifitHeader";
 import SenifitNavBar from "@/components/SenifitNavBar";
 import Container from "@mui/material/Container";
 import Footer from "./panel/Footer";
+import { Stack } from "@mui/material";
 
 export default function WithContainerLayout({
   children,
@@ -11,18 +12,21 @@ export default function WithContainerLayout({
   return (
     <>
       <SenifitHeader />
-      <Container
-        maxWidth={"desktop"}
-        sx={{
-          pt: [9, 12, 20.75],
-          px: [0, 3],
-          pb: [14, 15, 6],
-          boxSizing: "border-box",
-        }}
-      >
-        {children}
-      </Container>
-      <Footer />
+      <Stack direction={"column"} sx={{ minHeight: "100vh" }}>
+        <Container
+          maxWidth={"desktop"}
+          sx={{
+            pt: [9, 12, 20.75],
+            px: [0, 3],
+            pb: [14, 15, 6],
+            boxSizing: "border-box",
+            flexGrow: 1,
+          }}
+        >
+          {children}
+        </Container>
+        <Footer />
+      </Stack>
       <SenifitNavBar />
     </>
   );
