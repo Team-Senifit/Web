8482c76f3eee9c15296c136286d00c17792e1718
SaltySalt77 <hyna0707@gmail.com>
2025-09-12 01:05:40 +0900

    Feat: 멤버 관리 페이지 EmptyView 추가

--- src/app/(with-footer)/my-center/members/panel/MemberList.tsx
+++ src/app/(with-footer)/my-center/members/panel/MemberList.tsx
@@ -14,6 +14,7 @@ import {
 import dayjs from "dayjs";
 import Link from "next/link";
 import React, { useState } from "react";
+import EmptyView from "./EmptyView";
 
 const Member = ({
   memberId: id,
@@ -174,6 +175,7 @@ const MemberList = ({ members }: { members: Array<IMember> }) => {
           {...member}
         />
       ))}
+      {members.length === 0 && <EmptyView />}
     </>
   );
 };
