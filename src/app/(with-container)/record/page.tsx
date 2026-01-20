f889750103c8e4241ae10474dac35d899b54627c
HYUN-SIUU <hyunsoo6542@gmail.com>
2026-01-20 19:12:50 +0900

    fix: 인증 실패시 로그인 페이지로 리다이렉트 안되는 버그를 수정

--- src/app/(with-container)/record/page.tsx
+++ src/app/(with-container)/record/page.tsx
@@ -1,21 +1,11 @@
 import { Stack } from "@mui/material";
 import RecentRecordSection from "./panel/RecentRecordSection";
 import PastRecordsSection from "./panel/PastRecordsSection";
-import { redirect } from "next/navigation";
-import { isAuthError } from "@/apis/errors";
 import { createAxiosServer } from "@/apis/createAxiosServer";
 
 export default async function Record() {
-  try {
-    const api = await createAxiosServer();
-    await api.get("/records");
-  } catch (e) {
-    if (isAuthError(e)) {
-      const next = encodeURIComponent("/record");
-      redirect(`/login?next=${next}`);
-    }
-    throw e;
-  }
+  const api = await createAxiosServer();
+  await api.get("/records");
 
   return (
     <Stack spacing={3}>
