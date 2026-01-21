4f2c41bc978201bb40ad1e8aee389ac9e890639b
tmin002 <ssh9930@gmail.com>
2026-01-21 17:41:51 +0900

    Style: align formatting with CI Prettier
    Reformat files to match Prettier output used in CI (npm ci), fixing prettier/prettier lint errors.

--- src/components/SenifitToggleButtonGroupField.tsx
+++ src/components/SenifitToggleButtonGroupField.tsx
@@ -14,7 +14,9 @@ export interface ISenifitToggleButtonGroupFieldProps<
   T extends string | number | boolean,
   TFieldValues extends FieldValues = FieldValues,
   TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
-> extends Omit<ISenifitToggleButtonGroupProps<T>, "value" | "onChange">,
+>
+  extends
+    Omit<ISenifitToggleButtonGroupProps<T>, "value" | "onChange">,
     Pick<
       UseControllerProps<TFieldValues, TName>,
       "name" | "control" | "rules"
