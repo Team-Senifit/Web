209e1934846bdd37a45e171ceac4ed1eeaa3d14d
tmin002 <ssh9930@gmail.com>
2026-01-21 17:32:47 +0900

    Style: fix prettier lint after develop sync
    Apply Prettier formatting to match current lint rules.

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -22,10 +22,8 @@ export interface IVideoHandle {
   getEl: () => HTMLVideoElement | null;
 }
 
-export interface IVideoPlayerProps extends Omit<
-  React.VideoHTMLAttributes<HTMLVideoElement>,
-  "onTimeUpdate"
-> {
+export interface IVideoPlayerProps
+  extends Omit<React.VideoHTMLAttributes<HTMLVideoElement>, "onTimeUpdate"> {
   src: string;
   length: number;
   poster?: string;
