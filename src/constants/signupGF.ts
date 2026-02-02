732c0454c5a6dc4cac8ea5c1ba4089477b7c6a67
HYUN-SIUU <hyunsoo6542@gmail.com>
2026-02-02 09:56:57 +0900

    fix: 삭제된 어르신에 대한 처리

--- src/constants/signupGF.ts
+++ src/constants/signupGF.ts
@@ -1,3 +1,4 @@
-const signupGoogleForm = "https://docs.google.com/forms/d/e/1FAIpQLSePLIn6Y2hZkoadU2Jc8bHKzLPJtRZ47YSZ18Hiwy5ujjAUeA/viewform";
+const signupGoogleForm =
+  "https://docs.google.com/forms/d/e/1FAIpQLSePLIn6Y2hZkoadU2Jc8bHKzLPJtRZ47YSZ18Hiwy5ujjAUeA/viewform";
 
 export { signupGoogleForm };
